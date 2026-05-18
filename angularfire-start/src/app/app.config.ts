7658ac4260a81114c75628ee0557d88c7d2f4d1b
Hamakyo-eng <hamakyo19891126@gmail.com>
2026-05-19 00:13:33 +0900

    Install AngularFire

---
+++ angularfire-start/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth()),
+      provideFunctions(() => getFunctions()),
+      provideStorage(() => getStorage()),
+      provideMessaging(() => getMessaging())
+    ),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-51485","appId":"1:80526671329:web:e4030d913cf06e50bb66a0","storageBucket":"friendlychat-51485.firebasestorage.app","apiKey":"AIzaSyAyqLXuu1UA4TAb_598Pzs_ntN00NLBS1E","authDomain":"friendlychat-51485.firebaseapp.com","messagingSenderId":"80526671329","projectNumber":"80526671329","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+  ],
+};
